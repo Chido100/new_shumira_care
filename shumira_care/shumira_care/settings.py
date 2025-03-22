f029bbee453d30425fb8bf3e2832f288121707cd
Chido100 <chido_dozie@hotmail.com>
2025-03-22 18:21:06 +0000

    added new

--- shumira_care/shumira_care/settings.py
+++ shumira_care/shumira_care/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('MY_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('MY_DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['shumira.care', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['shumiracare.onrender.com','www.shumira.care', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -81,12 +81,16 @@ WSGI_APPLICATION = "shumira_care.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
 
 
 # Password validation
